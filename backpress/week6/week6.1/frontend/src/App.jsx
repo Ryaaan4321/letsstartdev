c98ec37d1a4c19499e614502f1039d9a81c8191d
Ryaaan4321 <aryan056400@gmail.com>
2024-11-07 08:17:32 +0530

    hmlo hmlo

--- backpress/week6/week6.1/frontend/src/App.jsx
+++ backpress/week6/week6.1/frontend/src/App.jsx
@@ -1,20 +1,46 @@
 import { useState } from "react";
-import Header1 from "./components/Header1";
-import Header2 from "./components/Header2";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <Header title="hello1" />
-      <Header title="hello2" />
+      <HeaderWithButton />
+      <br />
+      <Header title="my name is dooba dooba"></Header>
+    </>
+  );
+}
+
+function HeaderWithButton() {
+  const [firsttitle, setfirsttitle] = useState("my name is baba");
+  function changeTitle() {
+    setfirsttitle("my name is " + Math.random());
+  }
+  return (
+    <>
+      <button onClick={changeTitle}>click me to change the title</button>
+      <Header title={firsttitle}></Header>
     </>
   );
 }
 function Header({ title }) {
+  // console.log(title);
+  return <>{title}</>;
+}
+
+function initial() {
+  const [name1, setName1] = useState("raman");
+  const [name2] = useState("aman");
+
+  function handle() {
+    const newname = Math.floor(Math.random() * 100);
+    console.log(newname);
+    setName1(`random_${newname}`);
+  }
   return (
     <div>
-      {title}
+      <button onClick={handle}>Generate Name</button>
+      <p>My name is {name1}</p>
+      <p>My name is {name2}</p>
     </div>
   );
 }
